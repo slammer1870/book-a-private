6691d5fe2e29b24bf912fac89aca9738109bd5f0
Sam <sam.mcnally94@gmail.com>
2023-03-13 09:54:22 +0000

    add: create stripe connect account and assign to user
    when email verified

--- __tests__/api-routes/register.test.ts
+++ __tests__/api-routes/register.test.ts
@@ -11,7 +11,7 @@ const handler: typeof endpoint & { config?: PageConfig } = endpoint;
 const user = {
   name: "John Doe",
   email: "johndoe@gmail.com",
-  username: "jdoe1"
+  username: "jdoe1",
 };
 
 describe("testing user registration for endpoint /api/register/", () => {
@@ -42,7 +42,7 @@ describe("testing user registration for endpoint /api/register/", () => {
           body: JSON.stringify(user),
         });
         await expect(res.json()).resolves.toStrictEqual({
-          message: "User with this username or email already exists",
+          error: "User with this username or email already exists",
         }); // ◄ Passes!
       },
     });
