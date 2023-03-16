7450f412ba3482d40e6c17a61d328bc88ab5957b
Sam <sam.mcnally94@gmail.com>
2023-03-16 10:04:37 +0000

    add: calender with date selection

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,37 +1,207 @@
 import Layout from "@/components/Layout";
+import LinkStripe from "@/components/LinkStripe";
+import { User } from "next-auth";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+
+import { useState, useEffect } from "react";
 
 export default function Dashboard() {
   const router = useRouter();
 
-  const handleStripe = async () => {
-    const res = await fetch("/api/stripe/create-account-link", {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
+  const { data: session, status } = useSession();
+  const loading = status == "loading";
 
-    const data = await res.json();
+  if (!loading && !session) {
+    return router.push("/signin");
+  }
 
-    return router.push(data.url);
+  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedTime, setSelectedTime] = useState();
+  const [selectedInstructor, setSelectedInstructor] = useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [modal, setModal] = useState();
+
+  const [isLoading, setLoading] = useState(true);
+  const [schedules, setSchedules] = useState([]);
+  const [data, setData] = useState();
+
+  const [clientSecret, setClientSecret] = useState("");
+
+  const url = `https://${process.env.VERCEL_URL}` || "http://locahost:3000";
+
+  //get todays date
+  let d = new Date();
+
+  //set initial state of date object to be the value of todays date
+  const [today, setToday] = useState(d);
+
+  //array of strings that represent month names
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  //setting the initial state of the month string
+  const [month, setMonth] = useState();
+
+  //calculating the number of days in a month given the month and year
+  const getDays = (month: number, year: number) => {
+    const days = [];
+    var date = new Date(Date.UTC(year, month, 1));
+    while (date.getMonth() === month) {
+      days.push(new Date(date));
+      date.setDate(date.getDate() + 1);
+    }
+    return days;
   };
+
+  const [days, setDays] = useState(
+    getDays(today.getUTCMonth(), today.getUTCFullYear())
+  );
+
+  useEffect(() => {
+    setMonth(months[today.getUTCMonth()]);
+    setDays(getDays(today.getUTCMonth(), today.getUTCFullYear()));
+  }, [today]);
+
+  const calcArray = (day: number) => {
+    if (day > 1) {
+      return day - 1;
+    } else {
+      return 1;
+    }
+  };
+
   return (
     <Layout>
-      <div className="container mx-auto px-4 py-20">
+      <div className="max-w-screen-md mx-auto px-4 py-20">
         <h1 className="text-3xl font-medium mb-2">Dashboard</h1>
-        <p className="text-gray-700">
-          To complete your account we need to set up a way for you to get paid.
-          We have initiated the registration by creating a Stripe account for
-          you, we just need you to click the button below to complete the
-          process!
-        </p>
-        <div className="flex w-full my-12">
-          <button
-            onClick={() => handleStripe()}
-            className="w-full max-w-screen-sm mx-auto bg-indigo-400 hover:bg-indigo-500 text-white p-2 rounded"
-          >
-            Link your Stripe Account
-          </button>
-        </div>
+        {loading && <p className="text-gray-700 animate-pulse">Loading...</p>}
+        {!loading && session?.user.stripeAccountVerified ? (
+          <>
+            <p className="text-gray-700 mb-4">Welcome to your dashboard</p>
+            <div>
+              <h3 className="text-2xl font-semibold mb-4">
+                Manage your Bookings
+              </h3>
+              <div className="mb-4">
+                <div className="item-center mb-4 flex justify-center bg-gray-200 p-2 text-center">
+                  <span className="">Select a date</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="my-auto ml-4 h-3 w-3 rotate-90"
+                  >
+                    <path
+                      id="Polygon_17"
+                      data-name="Polygon 17"
+                      d="M6.5,0,13,11.762H0Z"
+                      transform="translate(11.762) rotate(90)"
+                      fill="#525252"
+                    />
+                  </svg>
+                </div>
+                <div className="mb-4 flex items-center justify-between px-12">
+                  {d.getMonth() < today.getMonth() ? (
+                    <button
+                      onClick={() =>
+                        setToday(new Date(today.setMonth(today.getMonth() - 1)))
+                      }
+                    >
+                      &#60;
+                    </button>
+                  ) : (
+                    <span></span>
+                  )}
+                  <p>{month}</p>
+                  {today.getMonth() - d.getMonth() < 2 ? (
+                    <button
+                      onClick={() =>
+                        setToday(new Date(today.setMonth(today.getMonth() + 1)))
+                      }
+                    >
+                      &#62;
+                    </button>
+                  ) : (
+                    <span></span>
+                  )}
+                </div>
+                <div className="mb-4 grid grid-cols-7 grid-rows-6 gap-4">
+                  <p className="col-span-1 mx-auto my-auto text-center text-xs font-semibold">
+                    Mon
+                  </p>
+                  <p className="col-span-1 mx-auto my-auto text-center text-xs font-semibold">
+                    Tues
+                  </p>
+                  <p className="col-span-1 mx-auto my-auto text-center text-xs font-semibold">
+                    Weds
+                  </p>
+                  <p className="col-span-1 mx-auto my-auto text-center text-xs font-semibold">
+                    Thurs
+                  </p>
+                  <p className="col-span-1 mx-auto my-auto text-center text-xs font-semibold">
+                    Fri
+                  </p>
+                  <p className="col-span-1 mx-auto my-auto text-center text-xs font-semibold">
+                    Sat
+                  </p>
+                  <p className="col-span-1 mx-auto my-auto text-center text-xs font-semibold">
+                    Sun
+                  </p>
+                  {[...Array(calcArray(days[0].getDay()))].map((x, i) => (
+                    <div
+                      className="mx-auto my-auto h-7 w-7 rounded-full"
+                      key={i}
+                    ></div>
+                  ))}
+                  <>
+                    {days.map((day) => (
+                      <>
+                        {day >= d ? (
+                          <button
+                            onClick={() => {
+                              setSelectedDate(day.toDateString());
+                              setSelectedTime(null);
+                              setSelectedInstructor(null);
+                            }}
+                            className={`${
+                              selectedDate &&
+                              selectedDate == day.toDateString() &&
+                              "border-none bg-indigo-400 text-white shadow-lg"
+                            } mx-auto my-auto flex h-7 w-7 cursor-pointer items-center justify-center rounded-full border border-gray-900`}
+                          >
+                            <p className="border-gray-900 text-xs ">
+                              {day.getDate()}
+                            </p>
+                          </button>
+                        ) : (
+                          <div className="mx-auto my-auto flex h-7 w-7 cursor-default items-center justify-center rounded-full border bg-white opacity-30">
+                            <p className="border-opacity-50 text-xs text-gray-900 ">
+                              {day.getDate()}
+                            </p>
+                          </div>
+                        )}
+                      </>
+                    ))}
+                  </>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <LinkStripe />
+        )}
       </div>
     </Layout>
   );
