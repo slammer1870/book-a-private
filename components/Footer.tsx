937c54960770dae8a21657d30de6dd94e82e1400
Sam Mc Nally <sam.mcnally94@gmail.com>
2024-02-29 14:54:43 +0000

    update: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="relative bottom-0 left-0 w-full bg-gray-50">
       <footer className="container mx-auto p-4 text-center text-sm">
-        <p className="font-light">This is the footer</p>
+        <span className="font-light">Website built by <a href="https://github.com/slammer1870" target="_blank" rel="noopener noreferrer" className="underline">Sam Mc Nally</a></span>
       </footer>
     </div>
   );
